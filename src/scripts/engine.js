429b1af336d9e15c91fac82abe1de9977dea39b7
Bruno Rodrigues <brunorodrigues1616@hotmail.com>
2024-01-22 23:27:36 -0300

    jogoDaMemoria
    Jogo da memoria, desafio da Dio, proposto por Felipe.
    acrescentado: Tempo, Tentativas

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -16,8 +16,55 @@ const emojis = [
   "🐮",
   "🐮",
 ];
-let openCards = [];
 
+let tempoTotal = 120; 
+    let cronometro;
+
+    function formatarTempo(tempo) {
+        const minutos = Math.floor(tempo / 60);
+        const segundos = tempo % 60;
+
+        return `${String(minutos).padStart(2, '0')}:${String(segundos).padStart(2, '0')}`;
+    }
+
+    function iniciarContagemRegressiva() {
+        cronometro = setInterval(function () {
+            tempoTotal--;
+            document.getElementById('cronometro').innerText = formatarTempo(tempoTotal);
+
+            if (tempoTotal <= 0) {
+                clearInterval(cronometro);
+                
+            }
+        }, 1000);
+    }
+
+    let tentativasRestantes = 50;
+
+    document.body.addEventListener('click', function () {
+        if (tentativasRestantes > 0) {
+            tentativasRestantes--;
+            document.getElementById('tentativas').innerText = tentativasRestantes;
+
+            if (tentativasRestantes === 0) {
+              
+                document.getElementById('mensagem').innerText = 'Você atingiu o limite de tentativas!';
+               var gameOver = document.getElementById('gameOver');
+               var game = document.getElementById('teste');
+               game.style.display = 'none';
+               gameOver.style.display = 'block';
+              
+                
+            }
+        }
+    });
+
+
+
+
+
+let openCards = [];
+playSound('hit');
 let shuffleEmojis = emojis.sort(() => (Math.random() > 0.5 ? 2 : -1));
 
 for (let i = 0; i < emojis.length; i++) {
@@ -32,6 +79,8 @@ function handleClick() {
   if (openCards.length < 2) {
     this.classList.add("boxOpen");
     openCards.push(this);
+    
+
   }
 
   if (openCards.length == 2) {
@@ -53,6 +102,18 @@ function checkMatch() {
   openCards = [];
 
   if (document.querySelectorAll(".boxMatch").length === emojis.length) {
-    alert("Você venceu !");
+    playSound('vitoria');
   }
+}
+
+function playSound(audioName) {
+  let audio = new Audio(`./src/audios/${audioName}.mp3`);
+  let vitoria = new Audio (`./src/audios/${audioName}.mp3`);
+  audio.volume = 0.1;
+  audio.play();
+  vitoria.play();
+}
+
+function reiniciarJogo(){
+  location.reload();
 }
